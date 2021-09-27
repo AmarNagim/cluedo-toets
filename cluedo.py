35d546c4d1a2e4999039398167de6f064b6d81c4
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-09-27 14:20:01 +0200

    next

--- cluedo.py
+++ cluedo.py
@@ -1,11 +1,14 @@
 # Amar Nagim
 
 # alle conclusies
-notPassedText = print('Je mag helaas niet op auditie.')
+notPassedText = 'Je mag helaas niet op auditie.'
+KolonelVanGeelen = 'Je mag op auditie voor de rol van Kolonel van Geelen.'
+DomineeGroenewoud = 'Je mag op auditie voor de rol van Dominee Groenewoud.'
+ProfessorPimpel = 'Je mag op auditie voor de rol van Professor Pimpel.'
+MwDeWit = 'Je mag op auditie voor de rol van Mevrouw de Wit.'
+RosaRoodhart = 'Je mag op auditie voor de rol van Rosa Roodhart.'
+BlaauwVanDraet = 'Je mag op auditie voor de rol van Mevrouw Blaauw van Draet.'
 
-
-KolonelVanGeelen = 'Je mag op auditie voor de rol van Kolonel van Geelen'
-KolonelVanGeelen = False
 # voor alle personages
 leeftijd = int(input('Hoe oud bent u?: '))
 verhuizen = input('Ben je bereid te verhuizen naar Amsterdam?: ').lower()
@@ -14,7 +17,7 @@ verhuizen = input('Ben je bereid te verhuizen naar Amsterdam?: ').lower()
 geslacht = input('Wat is uw geslacht?: ').lower()
 haarkleur = input('Wat is uw haarkleur?: ').lower()
 lengte = input('Wat is uw lengte?: ').lower()
-specialiteit = input('Wat is uw specialiteit in het acteren? (boos/onrechtmatig/bekend/slim):')
+specialiteit = input('Wat is uw specialiteit in het acteren? (bekend/slim/militair/acteur/geestelijke/koken):').lower()
 
 
 # conclusie
@@ -22,10 +25,13 @@ if leeftijd <18 or verhuizen != 'nee':
     print(notPassedText)
     exit
 
-if leeftijd <18 or verhuizen != 'nee':
-    notPassedText = True
+elif geslacht == 'man' and specialiteit == 'militair' and leeftijd >= 35 and leeftijd <= 60:    # Kolonel van Geelen
+    print(KolonelVanGeelen)
 
+elif geslacht == 'man' and specialiteit == 'geestelijke' and lengte >= 175 and lengte <= 195:    
+    print(DomineeGroenewoud)
 
+elif geslacht == 'man' and specialiteit == 'slim' and leeftijd >= 55
 
 
 
