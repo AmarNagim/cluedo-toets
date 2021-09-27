cce0a1f33fcd08762a6b75772af65e127e510a4c
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-09-27 14:42:20 +0200

    next

--- cluedo.py
+++ cluedo.py
@@ -1,5 +1,11 @@
 # Amar Nagim
 
+# initiele text
+print("""
+Er zullen u een aantal vragen worden gesteld die u naar waarheid moet beantwoorden.
+Daaruit zal blijken of je wel of niet op auditie wordt gevraagd.""")
+print()
+
 # alle conclusies
 notPassedText = 'Je mag helaas niet op auditie.'
 KolonelVanGeelen = 'Je mag op auditie voor de rol van Kolonel van Geelen.'
@@ -9,30 +15,50 @@ MwDeWit = 'Je mag op auditie voor de rol van Mevrouw de Wit.'
 RosaRoodhart = 'Je mag op auditie voor de rol van Rosa Roodhart.'
 BlaauwVanDraet = 'Je mag op auditie voor de rol van Mevrouw Blaauw van Draet.'
 
-# voor alle personages
+# algemene eisen
 leeftijd = int(input('Hoe oud bent u?: '))
 verhuizen = input('Ben je bereid te verhuizen naar Amsterdam?: ').lower()
-
+algemeneEisen = False
 # per personage
 geslacht = input('Wat is uw geslacht?: ').lower()
 haarkleur = input('Wat is uw haarkleur?: ').lower()
 lengte = input('Wat is uw lengte?: ').lower()
-specialiteit = input('Wat is uw specialiteit in het acteren? (bekend/slim/militair/acteur/geestelijke/koken):').lower()
+specialiteit = input('Wat is uw specialiteit in het acteren? (bekendheid/slim/militair/acteur/geestelijke/koken):').lower()
+
+
+if leeftijd <18 or verhuizen == 'nee':
+    algemeneEisen == True
 
 
 # conclusie
-if leeftijd <18 or verhuizen != 'nee':
-    print(notPassedText)
-    exit
 
-elif geslacht == 'man' and specialiteit == 'militair' and leeftijd >= 35 and leeftijd <= 60:    # Kolonel van Geelen
+if algemeneEisen == True:
+    print(notPassedText)
+
+elif geslacht == 'man' and specialiteit == 'militair' and leeftijd >= 35 and leeftijd <= 60:  # Kolonel van Geelen
+    print()  
     print(KolonelVanGeelen)
 
-elif geslacht == 'man' and specialiteit == 'geestelijke' and lengte >= 175 and lengte <= 195:    
+elif geslacht == 'man' and specialiteit == 'geestelijke' and lengte >= 175 and lengte <= 195:   
+    print()
     print(DomineeGroenewoud)
 
-elif geslacht == 'man' and specialiteit == 'slim' and leeftijd >= 55
+elif geslacht == 'man' and specialiteit == 'slim' and leeftijd >= 55:
+    print()
+    print(ProfessorPimpel)
 
+elif geslacht == 'vrouw' and specialiteit == 'koken' and leeftijd >= 40 and leeftijd <= 60:
+    print()
+    print(MwDeWit)
 
+elif geslacht == 'vrouw' and specialiteit == 'acteur' and leeftijd >= 20 and leeftijd <= 30:
+    print()
+    print(RosaRoodhart)
 
+elif geslacht == 'vrouw' and specialiteit == 'bekendheid' and leeftijd >25 and leeftijd <=40:
+    print()
+    print(BlaauwVanDraet)      
 
+else:
+    print()
+    print(notPassedText)
